d8782abc8363ff3c957032c4be27f05b05115d8d
Ehis-o-e <his.ehinomhen@gmail.com>
2025-10-25 13:52:51 +0100

    updated cors in events.gateway.ts

--- backend/src/websocket/events.gateway.ts
+++ backend/src/websocket/events.gateway.ts
@@ -16,7 +16,7 @@ import { LocationHistory } from '../entities/location-history.entity';
 
 @WebSocketGateway({
     cors: {
-        origin: 'http://localhost:3001',
+        origin: process.env.FRONTEND_URL || 'http://localhost:3001',
         credentials: true,
     },
 })
